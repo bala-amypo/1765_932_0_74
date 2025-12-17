db1d8fa288b7d0777b468d34af682a63b8a8ac0e
bala-amypo <bala@amypo.in>
2025-12-17 06:23:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,46 +1,52 @@
-package com.example.demo.entity;
+com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-public class StudentEntity{
+public class StudentEntity {
 
-@Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private float cgpa;
 
-private Long id;
-private String name;
-private String email;
-private float cgpa;
+    public StudentEntity(Long id, String name, String email, float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.cgpa = cgpa;
+    }
 
-public void setName(String name){
-    this.name=name;
-}
-public String getName(){
-    return this.name;
-}
-public void setEmail(String email){
-    this.email=email;
-}
-public String getEmail(){
-    return this.email;
-}
-public void setCgpa(float cgpa){
-    this.cgpa=cgpa;
-}
-public float getCgpa(){
-    return this.cgpa;
-}
-public StudentEntity(Long id,String name,String email,float cgpa){
-    this.name=name;
-    this.id=id;
-    this.email=email;
-    this.cgpa=cgpa;
-    this.id=id;
-}
-public StudentEntity(){
-}
+    public StudentEntity() {
+        
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public float getCgpa() {
+        return this.cgpa;
+    }
 }
