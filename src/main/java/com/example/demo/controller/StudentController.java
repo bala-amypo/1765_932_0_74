30457a1aae932a404a6238363561204341ab444f
bala-amypo <bala@amypo.in>
2025-12-17 08:49:43 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -19,5 +19,8 @@ public class StudentController {
     public StudentEntity addStudents(@RequestBody StudentEntity student){
         return ser.addStudents(student);
     }
-    
+    @GetMapping("/getStudents")
+    public List<StudentEntity> getStudents(){
+        return ser.getStudents();
+    }    
 }
